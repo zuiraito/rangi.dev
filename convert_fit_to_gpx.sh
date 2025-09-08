d8c7e264655dcb9d38992097edfeef874346f02d
zuiraito <116594953+zuiraito@users.noreply.github.com>
2025-09-08 11:55:46 +0200

    Update convert_fit_to_gpx.sh

--- convert_fit_to_gpx.sh
+++ convert_fit_to_gpx.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Get the directory of the script
 DIR="$(dirname "$0")"
 
-# Loop over all .fit files in that directory
-for f in "$DIR"/*.fit; do
-    # Skip if no .fit files exist
-    [ -e "$f" ] || continue
-    gpsbabel -i garmin_fit -f "$f" -o gtrnctr -F "${f%.fit}.tcx"
-done
+ARCHIVE="$DIR/fit archive"
+mkdir -p "$ARCHIVE"
 
+for f in "$DIR"/*.fit; do
+    [ -e "$f" ] || continue
+
+    gpsbabel -i garmin_fit -f "$f" -o gtrnctr -F "${f%.fit}.tcx"
+
+    if [ -f "${f%.fit}.tcx" ]; then
+        mv "$f" "$ARCHIVE/"
+    fi
+done
